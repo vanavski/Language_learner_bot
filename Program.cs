cb10f34b9b6c7b460fdc16309272f8d571cde3e2
Nikita Khokhlov <nkhokhlov11708@gmail.com>
2020-03-31 20:52:25 +0300

    Basic
    Добавлен базовый функционал (обработка нажатия на кнопку).

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LanguageBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //инициализация бота
+            Bot bot = new Bot();
+            //запуск
+            bot.TestApiAsync();
+            Console.ReadLine();
+        }
+    }
+}
